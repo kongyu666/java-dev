62736eb7967eda5360cc2da44a50c9be7ae83744
孔余 <2385569970@qq.com>
2024-08-08 10:13:59 +0800

    修改：MapStruct列表转换

--- MySpringMapStructPlus/src/test/java/local/kongyu/mapstruct/MapStructPlusApplicationTests.java
+++ MySpringMapStructPlus/src/test/java/local/kongyu/mapstruct/MapStructPlusApplicationTests.java
@@ -8,6 +8,9 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @SpringBootTest
 class MapStructPlusApplicationTests {
     @Autowired
@@ -18,6 +21,16 @@ class MapStructPlusApplicationTests {
         User user = new User("阿腾", 24, true, DateUtil.date().toTimestamp(), DateUtil.date());
         UserDTO userDTO = converter.convert(user, UserDTO.class);
         System.out.println(userDTO);
+    }
+
+    @Test
+    void converter02() {
+        ArrayList<User> list = new ArrayList<>() {{
+            add(new User("阿腾1", 24, true, DateUtil.date().toTimestamp(), DateUtil.date()));
+            add(new User("阿腾2", 25, false, DateUtil.date().toTimestamp(), DateUtil.date()));
+        }};
+        List<UserDTO> dtoList = converter.convert(list, UserDTO.class);
+        System.out.println(dtoList);
     }
 
 }
